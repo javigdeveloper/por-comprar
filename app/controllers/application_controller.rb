55cfe0c0e5c6b53547b2fa018990eb66758fb29c
Javier Garcia <javigdeveloper@gmail.com>
2025-10-03 05:42:34 -0500

    Add HTTP Basic Auth with test support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :http_basic_authenticate, unless: :skip_basic_auth?
+
+  private
+
+  def skip_basic_auth?
+    Rails.env.test?
+  end
+
+  def http_basic_authenticate
+    authenticate_or_request_with_http_basic("Por Comprar") do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
